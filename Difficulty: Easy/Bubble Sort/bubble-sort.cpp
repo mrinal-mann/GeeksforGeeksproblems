92ca2f7c8abc58166d2957f934414399fb2a48bb
Mrinal Manna <118587617+mrinal-mann@users.noreply.github.com>
2024-08-18 21:15:41 +0530

    optimal bubble-sort.cpp

--- Difficulty: Easy/Bubble Sort/bubble-sort.cpp
+++ Difficulty: Easy/Bubble Sort/bubble-sort.cpp
@@ -22,12 +22,17 @@ class Solution {
     // Function to sort the array using bubble sort algorithm.
     void bubbleSort(int arr[], int n) {
         for(int i=n-1;i>=0;i--){
+            int noswap=0;
             for(int j=0;j<=i-1;j++){
                 if(arr[j]>arr[j+1]){
                     int temp=arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
+                    noswap=1;
                 }
+            }
+            if(noswap==0){
+                break;
             }
         }
     }
