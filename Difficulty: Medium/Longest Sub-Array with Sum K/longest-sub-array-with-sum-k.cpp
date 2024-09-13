42bb0cb45e0148c93de639011327110472dc2164
Mrinal Manna <118587617+mrinal-mann@users.noreply.github.com>
2024-09-13 20:35:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Sub-Array with Sum K/longest-sub-array-with-sum-k.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+public:
+    int lenOfLongSubarr(int A[], int N, int K) {
+        map<long long, int> preSum; // To store prefix sum and its index
+        long long sum = 0; // To keep track of the prefix sum
+        int maxlen = 0; // To keep track of the maximum length of subarray
+
+        for (int i = 0; i < N; i++) {
+            sum += A[i]; // Update the prefix sum
+
+            // If the prefix sum equals K, update the maximum length
+            if (sum == K) {
+                maxlen = max(maxlen, i + 1);
+            }
+
+            // Calculate the remaining sum needed to reach K
+            long long rem = sum - K;
+
+            // If the remaining sum is found in the map, update the maximum length
+            if (preSum.find(rem) != preSum.end()) {
+                int len = i - preSum[rem];
+                maxlen = max(maxlen, len);
+            }
+
+            // If the prefix sum is not already in the map, add it
+            if (preSum.find(sum) == preSum.end()) {
+                preSum[sum] = i;
+            }
+        }
+
+        return maxlen; // Return the maximum length found
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	//code
+	
+	int t;cin>>t;
+	while(t--)
+	{
+	    int n, k;
+	    cin>> n >> k;
+	    int a[n];
+	    
+	    for(int i=0;i<n;i++)
+	        cin>>a[i];
+	   Solution ob;
+	   cout << ob.lenOfLongSubarr(a, n , k)<< endl;
+	    
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
